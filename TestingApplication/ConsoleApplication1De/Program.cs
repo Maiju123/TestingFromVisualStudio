3283caccb90d9866383eac7aadaaca9cb96aef2d
Maiju Schreck <K2322@student.jamk.fi>
2016-01-20 11:10:05 +0200

    demo1_T8kesken

---
+++ TestingApplication/ConsoleApplication1De/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1De
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna luku: ");
+            string line = Console.ReadLine();
+            int luku1 = int.Parse(line);
+
+            Console.WriteLine("Anna luku: ");
+            line = Console.ReadLine();
+            int luku2 = int.Parse(line);
+
+            Console.WriteLine("Anna luku: ");
+            line = Console.ReadLine();
+            int luku3 = int.Parse(line);
+        }
+    }
+}
