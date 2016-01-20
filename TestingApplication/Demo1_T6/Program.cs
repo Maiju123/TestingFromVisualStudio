7f6d8e30784990f48b5b8964082f58673e2ae6e2
Maiju Schreck <K2322@student.jamk.fi>
2016-01-20 11:03:21 +0200

    Demo1_T7

---
+++ TestingApplication/Demo1_T6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_T6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give distance of your trip: ");
+            string line = Console.ReadLine();
+            int kmeters = int.Parse(line);
+            double kulutus = kmeters * 7.02 / 100;
+            double bensa = kulutus * 1.595;
+
+            Console.WriteLine("You use {0} GAS, and it cost {1} euros.", bensa, kulutus);
+        }
+    }
+}
---
+++ TestingApplication/Demo1_T7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_T7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna vuosi: ");
+            string line = Console.ReadLine();
+            int year = int.Parse(line);
+            int vuosi = year / 4;
+            int taysvuosi = year / 4 * 100;
+            int vuosisadat = year / 400;
+
+            if (year % 4 == 0) {
+                if (vuosi % 100 == 0) {
+                    if (vuosi % 400 == 0) {
+                        Console.WriteLine("On karkausvuosi.");
+                    } else
+                    {
+                        Console.WriteLine("Ei ole karkausvuosi.");
+                    }
+                } else 
+                    {
+                        Console.WriteLine("On Karkausvuosi.");}
+            }
+            else 
+                    {
+                        Console.WriteLine("Ei ole karkausvuosi.");
+            }
+        }
+    }
+}
+
