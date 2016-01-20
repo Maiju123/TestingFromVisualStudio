94f5a6f28530ec8c7c3cbc00ffb9876828053825
Maiju Schreck <K2322@student.jamk.fi>
2016-01-20 09:41:16 +0200

    Added new project. Harjoitus4. Demo1.

---
+++ TestingApplication/Harjoitus4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give age: ");
+            string line = Console.ReadLine(); // "23"
+            int age = int.Parse(line); // 23
+            if (age < 18)
+            {
+                Console.WriteLine("You are under-age.");
+            } else if (age >=18 && age <=65)
+                {
+                Console.WriteLine("You are adult.");
+            } else
+            {
+                Console.WriteLine("You are senior.");
+            }
+            Console.WriteLine("Press enter key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
