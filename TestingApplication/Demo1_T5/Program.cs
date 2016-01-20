9dd588e890b12e19af110ef7beb726d10a796971
Maiju Schreck <K2322@student.jamk.fi>
2016-01-20 10:22:52 +0200

    Demo1_T5

---
+++ TestingApplication/Demo1_T5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give seconds: ");
+            string line = Console.ReadLine();
+            int secs = int.Parse(line);
+            int hour = secs / 3600;
+            int min = secs / 60 - hour * 60;
+            int sec = secs % 60;
+
+            Console.WriteLine("Time is: {0}hh {1}min {2}sec", hour, min, sec);
+        }
+        
+    }
+}
